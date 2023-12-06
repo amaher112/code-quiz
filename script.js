eb6a2ac1b27b55384d27b6a597642a18da9b35bd
amaher <amaher112@su.edu>
2023-12-05 22:06:35 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// Have a start button
+
+// Add ID and event listener to that button
+
+// Create a "start quiz" function
+
+// When the start button is clicked, a timer will appear and it will take you to the first question with answers
+
+// If I answer a question correctly, a message will appear and it will proceed to the next question.
+
+// If I answer a question incorrectly, a message will appear, it will proceed to the next question and time is subtracted from the timer.
+
+// The game is over either when all questions are answered or the timer reaches zero.
+
+// When the game is over, it will display the score equals to time left.
+
+// When the game is over, I can save my initial and my score.
