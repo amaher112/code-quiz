0738c1f68444f88698a835ffb152f39245d370c7
amaher <amaher112@su.edu>
2023-12-10 21:32:50 -0500

    added more CSS and JS

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -8,16 +8,25 @@ var scoreText = document.querySelector("#end-game");
 var finalScore = document.querySelector("#score");
 var submitBtn = document.querySelector("#submit");
 var initials = document.querySelector("#initials");
+// var startOverBtn = document.querySelector(".start-over");
+// var clearBtn = document.querySelector(".clear");
 
+// Array of questions and answers
 var questions = [
   {
-    question: "Which type of dialog box is used to display a message and a data entry field?",
+    question:
+      "Which type of dialog box is used to display a message and a data entry field?",
     answers: ["1. Alert ()", "2. Prompt()", "3. Confirm()", "4. Msg()"],
     correctAnswer: "2. Prompt()",
   },
   {
     question: "Which one of these represents a for loop?",
-    answers: ["1. ", "2. ", "3. ", "4. (i = 0; i < length; i++)"],
+    answers: [
+      "1. (i = 0; i++)",
+      "2. (i = length)",
+      "3. (i < length; i++)",
+      "4. (i = 0; i < length; i++)",
+    ],
     correctAnswer: "4. (i = 0; i < length; i++)",
   },
   {
@@ -27,16 +36,23 @@ var questions = [
   },
   {
     question: "What does the .split('') method do?",
-    answers: ["1. Turn a string into an array", "2. Turn objects into properties", "3. Turn an array into a string", "4. Turn a string into numbers"],
+    answers: [
+      "1. Turn a string into an array",
+      "2. Turn objects into properties",
+      "3. Turn an array into a string",
+      "4. Turn a string into numbers",
+    ],
     correctAnswer: "1. Turn a string into an array",
   },
 ];
 
 var currentQuestionIdx = 0;
 var timeLeft = 75;
+var timeInterval
 
 function startQuiz() {
-  var timeInterval = setInterval(function () {
+  // Starts the timer and ends the game when it reaches "0"
+  timeInterval = setInterval(function () {
     timeLeft--;
     timeEl.textContent = "Time: " + timeLeft;
     if (timeLeft === 0) {
@@ -44,6 +60,8 @@ function startQuiz() {
       endGame();
     }
   }, 1000);
+
+  // The start button and instructions disappear, and the first questions object appears.
   startBtn.style.display = "none";
   introEl.style.display = "none";
   quizEl.style.display = "block";
@@ -52,10 +70,17 @@ function startQuiz() {
 }
 
 function nextQuestion() {
+  // Checks to see if there are any more questions in the 'questions' array.
+  if (currentQuestionIdx >= questions.length) {
+    endGame();
+  }
+  // Clears the answer buttons from the previous question
   answerText.textContent = "";
   var currentQuestion = questions[currentQuestionIdx].question;
   var currentAnswer = questions[currentQuestionIdx].answers;
   questionText.textContent = currentQuestion;
+
+  // The for loop adds the answers to the screen as buttons
   for (var i = 0; i < currentAnswer.length; i++) {
     var answerBtn = document.createElement("button");
     answerBtn.setAttribute("class", "answers-text");
@@ -65,56 +90,58 @@ function nextQuestion() {
   }
 }
 
+// Hides the quiz questions and shows final score.
 function endGame() {
+  clearInterval(timeInterval);
+  timeEl.style.display = "none";
   quizEl.style.display = "none";
   scoreText.style.display = "block";
   finalScore.textContent = "Your final score is: " + timeLeft;
 }
 
+// Gets values of endScore and initials, and displays them as highscores.
 function displayScore() {
   var value = localStorage.getItem("endScore");
+  initials = localStorage.getItem("initials");
   var displayScore = document.getElementById("display");
-  displayScore.textContent = value;
-
-  //  display "highscore"
+  var highScore = document.querySelector("#highscore");
+  highScore.style.display = "block";
+  displayScore.textContent = initials + " - " + value;
+  
   // Add clear and go back buttons
 }
 
-// If I answer a question,  it will proceed to the next question.
-
-// If I answer a question incorrectly, time is subtracted from the timer.
-
-// The game is over either when all questions are answered or the timer reaches zero.
-
-// When the game is over, it will display the score equals to time left.
-
-// When the game is over, I can save my initial and my score.
-
+// Starts the game
 startBtn.addEventListener("click", function () {
   startQuiz();
 });
 
+// Checks to see if what the user clicked matches the correct answer
 answerText.addEventListener("click", function (e) {
-  var clickedBtn = e.target;
   var clickedBtnValue = e.target.value;
-  console.log(clickedBtnValue);
   if (clickedBtnValue === questions[currentQuestionIdx].correctAnswer) {
     currentQuestionIdx++;
     nextQuestion();
   } else {
-    // Subtract time
+    // Subtracts time for an incorrect answer
     function subtractTime() {
       timeLeft -= 10;
     }
+    subtractTime();
     currentQuestionIdx++;
     nextQuestion();
   }
 });
 
+// After the user enters their info, their initials and highscore will be stored and displayed.
 submitBtn.addEventListener("click", function () {
+  event.preventDefault();
   scoreText.style.display = "none";
   finalScore.textContent = "";
   localStorage.setItem("endScore", timeLeft);
   localStorage.setItem("initials", initials.value);
   displayScore();
 });
+
+// startOverBtn.addEventListener("click", startQuiz());
+// clearBtn.addEventListener("click", localStorage.clear());
